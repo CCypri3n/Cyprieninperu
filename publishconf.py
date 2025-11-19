c12e558a58851dd625d855a7345e6a2e4b742d7c
Cyprien <108684496+CCypri3n@users.noreply.github.com>
2025-11-19 12:12:22 -0500

    First site build for netlify in __site directory.

--- publishconf.py
+++ publishconf.py
@@ -8,13 +8,15 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = ""
+SITEURL = 'https://cyprieninperu.netlify.app'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 
 DELETE_OUTPUT_DIRECTORY = True
+
+OUTPUT_PATH = '__site/'
 
 # Following items are often useful when publishing
 
